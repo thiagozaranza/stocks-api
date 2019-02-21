9342130611f754995de85ab00dc0d6c39cc28fc4
Thiago Zaranza <thiagozaranza@gmail.com>
2019-02-21 16:17:03 -0300

    create_empresas_table migration

--- database/migrations/2019_02_21_002840_create_empresas_table.php
+++ database/migrations/2019_02_21_002840_create_empresas_table.php
@@ -15,7 +15,27 @@ class CreateEmpresasTable extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('segmento_id');
+            $table->string('nome', 32)->unique();
+            $table->string('codigo', 64)->unique();
+            $table->string('segmento', 3)->unique();
+            $table->string('razao_social', 32)->unique();
+            $table->string('cnpj', 18)->unique();
+            $table->string('nire', 15)->unique();
+            $table->string('isin', 15)->unique();
+            $table->string('cvm', 10)->unique();
+            $table->string('site', 64);
+            $table->string('pais', 16);
+            $table->string('atividade', 256);
+            $table->date('data_constituicao');
+            $table->date('data_registro_cvm');
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by');
             $table->timestamps();
+
+            $table->foreign('segmento_id')->references('id')->on('segmentos');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
         });
     }
 
